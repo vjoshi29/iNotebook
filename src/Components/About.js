6f42caab4aee156586ad413004b00e7abc7d1686
Vaishali Joshi <140043392+vjoshi29@users.noreply.github.com>
2024-03-18 20:26:40 +0530

    Create About.js

---
+++ src/Components/About.js
@@ -0,0 +1,18 @@
+
+import React from 'react'
+
+const About = () => {
+    
+  return (
+    <>
+    <div className='container' style={{width:"50%"}}>
+      <img  src="https://images.pexels.com/photos/7319184/pexels-photo-7319184.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" style={{height: '60%' , width:'100%'}}/>
+      
+    </div>
+    <br/>
+    <h3 className='container py-2' style={{width:"55vw", height:"max-content" ,backgroundColor:"#9b273c" , borderRadius:"10px" , boxShadow:"0px 5px 15px gray" ,fontSize:"2vw" ,color:"#ffffff" , fontFamily:"times new roman"}}>An e-notes app where you can store all your day to day notes in an easy and safe manner!</h3>
+</>
+  )
+}
+
+export default About
